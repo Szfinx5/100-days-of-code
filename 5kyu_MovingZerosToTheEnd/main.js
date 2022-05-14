131d6b61f567edd1369160290c1bf58ce71316a7
ghavasi <g.havasi@outlook.com>
2022-05-14 14:53:59 +0100

    Day 3 - 5kyu_Moving Zeros To The End - Completed

---
+++ 5kyu_MovingZerosToTheEnd/main.js
@@ -0,0 +1,29 @@
+console.log("Working...");
+
+/*
+Write an algorithm that takes an array and moves all of the zeros to the end, 
+preserving the order of the other elements.
+
+moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+*/
+
+function moveZeros(arr) {
+  newArray = [];
+  countZeros = 0;
+  console.log(arr.length)
+  for (let i=0;i<arr.length;i++){
+    if (arr[i] === 0){
+      countZeros++
+    } else{
+      newArray.push(arr[i])
+    }
+  }console.log(countZeros)
+
+  for (let i=0;i<countZeros;i++){
+    newArray.push(0);
+  }
+  return newArray;
+}
+
+// console.log(moveZeros[1,2,0,1,0,1,0,3,0,1]);
+console.log(moveZeros([1,2,0,1,0,1,0,3,0,1]));
